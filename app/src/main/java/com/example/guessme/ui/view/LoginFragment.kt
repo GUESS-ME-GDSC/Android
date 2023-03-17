3b8954ff485b7e52a78d5625bc19425f0c6e59c3
915dbfl <dbfl00915@naver.com>
2023-03-17 18:38:13 +0900

    [Feat] #3 로그인 화면 구성(로직x)

---
+++ app/src/main/java/com/example/guessme/ui/view/LoginFragment.kt
@@ -0,0 +1,20 @@
+package com.example.guessme.ui.view
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.guessme.R
+import com.example.guessme.common.base.BaseFragment
+import com.example.guessme.databinding.FragmentLoginBinding
+
+class LoginFragment(layoutResId: Int) : BaseFragment<FragmentLoginBinding>(R.layout.fragment_login) {
+    override fun getFragmentBinding(
+        inflater: LayoutInflater,
+        container: ViewGroup?
+    ): FragmentLoginBinding {
+        return FragmentLoginBinding.inflate(inflater, container, false)
+    }
+
+}
