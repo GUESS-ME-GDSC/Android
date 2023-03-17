c7db37589d3dd5473776ff6ff601aad4179c11fd
915dbfl <dbfl00915@naver.com>
2023-03-17 16:42:27 +0900

    [Chore] #1 프로젝트 패키징

--- app/src/main/java/com/example/guessme/ui/view/MainActivity.kt
+++ app/src/main/java/com/example/guessme/ui/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.guessme
+package com.example.guessme.ui.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.guessme.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
---
+++ app/src/main/java/com/example/guessme/ui/viewmodel/readme.md
@@ -0,0 +1,1 @@
+## viewModel package
