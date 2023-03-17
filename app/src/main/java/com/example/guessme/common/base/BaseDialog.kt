5cd5bddb59d41267af00f498c468ad702a4804e1
915dbfl <dbfl00915@naver.com>
2023-03-17 17:03:39 +0900

    [Feat] #1 base fragment, dialog 생성

--- app/src/main/java/com/example/guessme/common/base/BaseDialog.kt
+++ app/src/main/java/com/example/guessme/common/base/BaseDialog.kt
@@ -1,4 +1,45 @@
 package com.example.guessme.common.base
 
-class BaseDialog {
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import android.os.Bundle
+import android.view.*
+import androidx.fragment.app.DialogFragment
+import androidx.viewbinding.ViewBinding
+
+abstract class BaseDialog<T: ViewBinding> (private val layoutResId: Int) : DialogFragment() {
+
+    private var _binding: T? = null
+    protected val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        dialog?.apply {
+            window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+            requestWindowFeature(Window.FEATURE_NO_TITLE)
+        }
+
+        _binding = getViewBinding()
+        return binding.root
+    }
+
+    abstract fun getViewBinding(): T
+
+    override fun onResume() {
+        super.onResume()
+        val display = resources.displayMetrics
+        val window: Window = dialog?.window ?: return
+        val params: WindowManager.LayoutParams = window.attributes
+        params.width = (display.widthPixels * 0.8).toInt()
+
+        dialog?.window!!.attributes = params
+    }
+
+    override fun onDestroy() {
+        _binding = null
+        super.onDestroy()
+    }
+
 }
---
+++ app/src/main/java/com/example/guessme/common/base/BaseFragment.kt
@@ -0,0 +1,35 @@
+package com.example.guessme.common.base
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.viewbinding.ViewBinding
+
+abstract class BaseFragment<T: ViewBinding>(private val layoutResId: Int): Fragment() {
+
+    private var _binding: T? = null
+    protected val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = getFragmentBinding(inflater, container)
+        return binding.root
+    }
+
+    abstract fun getFragmentBinding(inflater: LayoutInflater, container: ViewGroup?): T
+
+    override fun onDestroy() {
+        _binding = null
+        super.onDestroy()
+    }
+
+    protected fun showToast(message: String) {
+        Toast.makeText(context, message, Toast.LENGTH_SHORT).show()
+    }
+}
