e25bca74d8a18a000e3626a420a67aec1ac78881
915dbfl <dbfl00915@naver.com>
2023-03-19 01:05:01 +0900

    [Feat] #7 home 화면 구성(로직x)

---
+++ app/src/main/java/com/example/guessme/ui/view/HomeFragment.kt
@@ -0,0 +1,20 @@
+package com.example.guessme.ui.view
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.guessme.R
+import com.example.guessme.common.base.BaseFragment
+import com.example.guessme.databinding.FragmentHomeBinding
+
+class HomeFragment : BaseFragment<FragmentHomeBinding>(R.layout.fragment_home) {
+    override fun getFragmentBinding(
+        inflater: LayoutInflater,
+        container: ViewGroup?
+    ): FragmentHomeBinding {
+        return FragmentHomeBinding.inflate(inflater, container, false)
+    }
+
+}
