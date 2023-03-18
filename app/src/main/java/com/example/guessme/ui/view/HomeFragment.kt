577c67bb3818bd6d047b4eeec2315242c44848a8
915dbfl <dbfl00915@naver.com>
2023-03-19 01:46:38 +0900

    [Design] 앱바 색 변경 및 홈화면부터 표시

--- app/src/main/java/com/example/guessme/ui/view/HomeFragment.kt
+++ app/src/main/java/com/example/guessme/ui/view/HomeFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
 import com.example.guessme.R
 import com.example.guessme.common.base.BaseFragment
 import com.example.guessme.databinding.FragmentHomeBinding
@@ -15,6 +16,11 @@ class HomeFragment : BaseFragment<FragmentHomeBinding>(R.layout.fragment_home) {
         container: ViewGroup?
     ): FragmentHomeBinding {
         return FragmentHomeBinding.inflate(inflater, container, false)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        (activity as AppCompatActivity)!!.supportActionBar!!.show()
     }
 
 }
