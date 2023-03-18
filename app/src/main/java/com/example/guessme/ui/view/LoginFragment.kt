3ad885422bac21c0891e9313ecd4d6fd2b2ab65d
915dbfl <dbfl00915@naver.com>
2023-03-18 17:01:22 +0900

    [Feat] #9 login 화면 생성자 코드 수정 및 액션바 숨김

--- app/src/main/java/com/example/guessme/ui/view/LoginFragment.kt
+++ app/src/main/java/com/example/guessme/ui/view/LoginFragment.kt
@@ -5,16 +5,27 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
 import com.example.guessme.R
 import com.example.guessme.common.base.BaseFragment
 import com.example.guessme.databinding.FragmentLoginBinding
 
-class LoginFragment(layoutResId: Int) : BaseFragment<FragmentLoginBinding>(R.layout.fragment_login) {
+class LoginFragment: BaseFragment<FragmentLoginBinding>(R.layout.fragment_login) {
     override fun getFragmentBinding(
         inflater: LayoutInflater,
         container: ViewGroup?
     ): FragmentLoginBinding {
         return FragmentLoginBinding.inflate(inflater, container, false)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        (activity as AppCompatActivity?)!!.supportActionBar!!.hide()
+    }
+
+    override fun onStop() {
+        (activity as AppCompatActivity?)!!.supportActionBar!!.show()
+        super.onStop()
     }
 
 }
