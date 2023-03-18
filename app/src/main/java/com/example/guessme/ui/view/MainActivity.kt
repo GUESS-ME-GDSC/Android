5495c71e5a26173b57f5e56820d48e0e978ba717
915dbfl <dbfl00915@naver.com>
2023-03-18 16:52:57 +0900

    [Feat] #9 main activity 화면 구성 수정 및 navigation 연결

--- app/src/main/java/com/example/guessme/ui/view/MainActivity.kt
+++ app/src/main/java/com/example/guessme/ui/view/MainActivity.kt
@@ -3,6 +3,9 @@ package com.example.guessme.ui.view
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
 import com.example.guessme.R
 import com.example.guessme.databinding.ActivityMainBinding
 
@@ -10,8 +13,11 @@ class MainActivity : AppCompatActivity() {
      private val binding: ActivityMainBinding by lazy {
         ActivityMainBinding.inflate(layoutInflater)
     }
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         installSplashScreen()
         super.onCreate(savedInstanceState)
+        setContentView(binding.root)
     }
 }
