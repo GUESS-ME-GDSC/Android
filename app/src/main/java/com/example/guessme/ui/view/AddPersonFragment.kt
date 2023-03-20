f45a082595b612c0902348ef3ba32aa02bd7fd8a
915dbfl <dbfl00915@naver.com>
2023-03-21 02:35:58 +0900

    [Feat] #19 음성 녹음 및 스피커 기능 구현

--- app/src/main/java/com/example/guessme/ui/view/AddPersonFragment.kt
+++ app/src/main/java/com/example/guessme/ui/view/AddPersonFragment.kt
@@ -1,17 +1,24 @@
 package com.example.guessme.ui.view
 
+import android.media.MediaPlayer
+import android.media.MediaRecorder
+import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.annotation.RequiresApi
 import androidx.fragment.app.viewModels
 import com.example.guessme.R
 import com.example.guessme.common.base.BaseFragment
 import com.example.guessme.common.util.Constants.REQUIRED_GALLERY_PERMISSION
+import com.example.guessme.common.util.Constants.REQUIRED_RECORD_AUDIO
 import com.example.guessme.databinding.FragmentAddPersonBinding
-import com.example.guessme.ui.dialog.PermissionDialog
+import com.example.guessme.ui.dialog.NoticeDialog
 import com.example.guessme.ui.viewmodel.AddPersonViewModel
+import java.io.IOException
 
 class AddPersonFragment : BaseFragment<FragmentAddPersonBinding>(R.layout.fragment_add_person) {
     private val addPersonViewModel by viewModels<AddPersonViewModel>()
@@ -23,11 +30,34 @@ class AddPersonFragment : BaseFragment<FragmentAddPersonBinding>(R.layout.fragme
         return FragmentAddPersonBinding.inflate(inflater, container, false)
     }
 
+    @RequiresApi(Build.VERSION_CODES.S)
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         binding.viewAddPersonForImage.setOnClickListener {
             requestGalleryLauncher.launch(REQUIRED_GALLERY_PERMISSION)
+        }
+
+        binding.btnAddPersonRecordStart.setOnClickListener {
+            requestRecordLauncher.launch(REQUIRED_RECORD_AUDIO)
+        }
+
+        binding.btnAddPersonRecordStop.setOnClickListener {
+            stopRecording()
+        }
+
+        binding.btnAddPersonSpeaker.setOnClickListener {
+            startPlaying()
+        }
+
+        addPersonViewModel.recordStatus.observe(viewLifecycleOwner) { status ->
+            if (status) {
+                binding.btnAddPersonRecordStart.visibility = View.GONE
+                binding.btnAddPersonRecordStop.visibility = View.VISIBLE
+            } else {
+                binding.btnAddPersonRecordStart.visibility = View.VISIBLE
+                binding.btnAddPersonRecordStop.visibility = View.GONE
+            }
         }
     }
 
@@ -36,7 +66,7 @@ class AddPersonFragment : BaseFragment<FragmentAddPersonBinding>(R.layout.fragme
         if (isGranted) {
             getGalleryLauncher.launch("image/*")
         } else {
-            val dialog = PermissionDialog()
+            val dialog = NoticeDialog(R.string.dialog_permission)
             dialog.show(requireActivity().supportFragmentManager, "PermissionDialog")
         }
     }
@@ -48,4 +78,88 @@ class AddPersonFragment : BaseFragment<FragmentAddPersonBinding>(R.layout.fragme
             binding.imageAddPersonProfile.setImageURI(uri)
         }
     }
+
+    @RequiresApi(Build.VERSION_CODES.S)
+    private val requestRecordLauncher = registerForActivityResult(
+        ActivityResultContracts.RequestPermission()) { isGranted ->
+        if (isGranted) {
+            startRecording()
+        } else {
+            val dialog = NoticeDialog(R.string.dialog_permission)
+            dialog.show(requireActivity().supportFragmentManager, "PermissionDialog")
+        }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.S)
+    private fun startRecording() {
+        addPersonViewModel.setFileName("${requireActivity().externalCacheDir!!.absolutePath}/recording.mp3")
+
+        val recorder: MediaRecorder = if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            MediaRecorder(requireContext())
+        } else {
+            MediaRecorder()
+        }
+
+        addPersonViewModel.setRecorder(recorder)
+
+        addPersonViewModel.recorder.apply {
+            setAudioSource(MediaRecorder.AudioSource.MIC)
+            setOutputFormat(MediaRecorder.OutputFormat.MPEG_4)
+            setOutputFile(addPersonViewModel.fileName)
+            setAudioEncoder(MediaRecorder.AudioEncoder.AAC)
+            setAudioSamplingRate(48000)
+            setAudioEncodingBitRate(128000)
+
+            try {
+                prepare()
+            } catch (e: IOException) {
+                Log.e("IOException", "prepare() failed")
+            }
+
+            addPersonViewModel.setRecordStatus(true)
+            start()
+        }
+
+    }
+
+    private fun stopRecording() {
+        addPersonViewModel.recorder.apply {
+            stop()
+            reset()
+            release()
+            addPersonViewModel.setRecordStatus(false)
+        }
+        addPersonViewModel.setRecorder(null)
+    }
+
+    private fun startPlaying() {
+        addPersonViewModel.setPlayer(MediaPlayer())
+
+        addPersonViewModel.player.setOnCompletionListener {
+            addPersonViewModel.player.apply {
+                stop()
+                reset()
+                release()
+            }
+
+            addPersonViewModel.setPlayer(null)
+        }
+
+        addPersonViewModel.player.apply {
+            try {
+                setDataSource(addPersonViewModel.fileName)
+                prepare()
+                start()
+            } catch (e: IOException) {
+                Log.e("IOException", "prepare() failed")
+                val dialog = NoticeDialog(R.string.dialog_record_error)
+                dialog.show(requireActivity().supportFragmentManager, "RecordDialog")
+            } catch (e: java.lang.RuntimeException) {
+                Log.e("RuntimeException", "runtime exception")
+                val dialog = NoticeDialog(R.string.dialog_record_no_file)
+                dialog.show(requireActivity().supportFragmentManager, "RecordDialog")
+            }
+        }
+    }
+
 }
--- app/src/main/java/com/example/guessme/ui/viewmodel/AddPersonViewModel.kt
+++ app/src/main/java/com/example/guessme/ui/viewmodel/AddPersonViewModel.kt
@@ -1,23 +1,43 @@
 package com.example.guessme.ui.viewmodel
 
+import android.media.MediaPlayer
+import android.media.MediaRecorder
 import android.net.Uri
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import java.io.File
 
 class AddPersonViewModel: ViewModel() {
     private var _imageUri: Uri? = null
-    private val imageUri get() = _imageUri!!
-    private var _audioUri: Uri? = null
-    private val audioUri get() = _audioUri!!
+    val imageUri get() = _imageUri!!
+    val audioUri get() = Uri.fromFile(File(fileName))
+    private var _player: MediaPlayer? = null
+    val player get() = _player!!
+    private var _recorder: MediaRecorder? = null
+    val recorder get() = _recorder!!
+    private var _recordStatus = MutableLiveData<Boolean>()
+    val recordStatus: LiveData<Boolean> get() = _recordStatus!!
+    private var _fileName: String? = null
+    val fileName get() = _fileName!!
 
     fun setImage(image: Uri) {
         _imageUri = image
     }
 
-    fun getImage() = imageUri
-
-    fun setAudio(audio: Uri) {
-        _audioUri = audio
+    fun setPlayer(player: MediaPlayer?) {
+        _player = player
     }
 
-    fun getAudio() = audioUri
+    fun setRecorder(recorder: MediaRecorder?) {
+        _recorder = recorder
+    }
+
+    fun setRecordStatus(status: Boolean) {
+        _recordStatus.postValue(status)
+    }
+
+    fun setFileName(name: String) {
+        _fileName = name
+    }
 }
