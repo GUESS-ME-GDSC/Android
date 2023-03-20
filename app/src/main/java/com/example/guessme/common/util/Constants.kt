680f65d56a8cb9b7ddeec25da678f0ec03730d31
915dbfl <dbfl00915@naver.com>
2023-03-20 21:12:16 +0900

    [Feat] #19 필요한 퍼미션 추가 및 정의

---
+++ app/src/main/java/com/example/guessme/common/util/Constants.kt
@@ -0,0 +1,6 @@
+package com.example.guessme.common.util
+
+object Constants {
+    const val REQUIRED_GALLERY_PERMISSION = android.Manifest.permission.READ_MEDIA_IMAGES
+    const val REQUIRED_RECORD_AUDIO = android.Manifest.permission.RECORD_AUDIO
+}
