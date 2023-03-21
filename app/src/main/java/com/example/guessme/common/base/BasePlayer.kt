a1984e791f2b4c0937928f7ac8b41bf169880255
915dbfl <dbfl00915@naver.com>
2023-03-21 22:22:38 +0900

    [Feat] BaseRecorder, BasePlayer 정의

---
+++ app/src/main/java/com/example/guessme/common/base/BasePlayer.kt
@@ -0,0 +1,47 @@
+package com.example.guessme.common.base
+
+import android.media.MediaPlayer
+import android.util.Log
+import androidx.fragment.app.FragmentManager
+import com.example.guessme.R
+import com.example.guessme.ui.dialog.NoticeDialog
+import java.io.IOException
+
+class BasePlayer(private val fragmentManager: FragmentManager) {
+    private var _player: MediaPlayer? = null
+    private val player get() = _player!!
+
+
+
+    fun setPlayer(player: MediaPlayer) {
+        _player = player
+    }
+
+    fun startPlaying(fileName: String?) {
+
+        player.setOnCompletionListener {
+            player.apply {
+                stop()
+                reset()
+                release()
+                _player = null
+            }
+        }
+
+        player.apply {
+            try {
+                setDataSource(fileName)
+                prepare()
+                start()
+            } catch (e: IOException) {
+                Log.e("IOException", "prepare() failed")
+                val dialog = NoticeDialog(R.string.dialog_record_error)
+                dialog.show(fragmentManager, "RecordDialog")
+            } catch (e: java.lang.RuntimeException) {
+                Log.e("RuntimeException", "runtime exception")
+                val dialog = NoticeDialog(R.string.dialog_record_no_file)
+                dialog.show(fragmentManager, "RecordDialog")
+            }
+        }
+    }
+}
