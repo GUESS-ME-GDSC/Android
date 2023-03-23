8b9115d39988071bc54780fa63f9f1944fb9efcb
915dbfl <dbfl00915@naver.com>
2023-03-23 12:46:26 +0900

    [Feat] #26 퀴즈 시작 화면 로직 구현

--- app/src/main/java/com/example/guessme/ui/view/StartQuizFragment.kt
+++ app/src/main/java/com/example/guessme/ui/view/StartQuizFragment.kt
@@ -1,20 +1,49 @@
 package com.example.guessme.ui.view
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.navArgs
 import com.example.guessme.R
 import com.example.guessme.common.base.BaseFragment
 import com.example.guessme.databinding.FragmentStartQuizBinding
+import com.example.guessme.ui.viewmodel.StartQuizViewModel
 
 class StartQuizFragment : BaseFragment<FragmentStartQuizBinding>(R.layout.fragment_start_quiz) {
+    private val startQuizFragmentArgs: StartQuizFragmentArgs by navArgs()
+    private val startQuizViewModel by viewModels<StartQuizViewModel>()
+
     override fun getFragmentBinding(
         inflater: LayoutInflater,
         container: ViewGroup?
     ): FragmentStartQuizBinding {
         return FragmentStartQuizBinding.inflate(inflater, container, false)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        startQuizViewModel.setPerson(startQuizFragmentArgs.person)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        init()
+    }
+
+    private fun init() {
+        val person = startQuizViewModel.person
+
+        person.image?.let {
+            binding.imageQuizProfile.setImageURI(it)
+        }
+
+        binding.btnQuizStart.setOnClickListener {
+            //info 넘겨줌
+            //퀴즈 화면으로 이동 필요
+        }
     }
 
 
--- app/src/main/java/com/example/guessme/ui/viewmodel/StartQuizViewModel.kt
+++ app/src/main/java/com/example/guessme/ui/viewmodel/StartQuizViewModel.kt
@@ -1,4 +1,14 @@
 package com.example.guessme.ui.viewmodel
 
-class StartQuizViewModel {
+import androidx.lifecycle.ViewModel
+import com.example.guessme.data.model.Person
+
+class StartQuizViewModel: ViewModel() {
+    private var _person: Person? = null
+    val person get() = _person!!
+
+    fun setPerson(data: Person) {
+        _person = data
+    }
+
 }
