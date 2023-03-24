cb8fd00a96ea0bf008d2a64e9b39fc02871d374f
915dbfl <dbfl00915@naver.com>
2023-03-25 02:51:44 +0900

    [Feat] #29 로그인, 회원가입 로직 구현

--- app/src/main/java/com/example/guessme/ui/view/LoginFragment.kt
+++ app/src/main/java/com/example/guessme/ui/view/LoginFragment.kt
@@ -1,17 +1,27 @@
 package com.example.guessme.ui.view
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import com.example.guessme.R
 import com.example.guessme.common.base.BaseFragment
+import com.example.guessme.data.model.User
 import com.example.guessme.databinding.FragmentLoginBinding
+import com.example.guessme.ui.dialog.ErrorDialog
+import com.example.guessme.ui.dialog.NoticeDialog
+import com.example.guessme.ui.viewmodel.LoginViewModel
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import java.lang.Exception
 
 class LoginFragment: BaseFragment<FragmentLoginBinding>(R.layout.fragment_login) {
+    private val logInViewModel: LoginViewModel by viewModels()
+
     override fun getFragmentBinding(
         inflater: LayoutInflater,
         container: ViewGroup?
@@ -22,13 +32,44 @@ class LoginFragment: BaseFragment<FragmentLoginBinding>(R.layout.fragment_login)
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.btnLoginSignIn.setOnClickListener {
+        binding.btnLoginSignUp.setOnClickListener {
             findNavController().navigate(R.id.action_fragment_login_to_signUpFragment)
         }
 
         binding.btnLogin.setOnClickListener {
-            //user 정보 전달 필요
-            findNavController().navigate(R.id.action_fragment_login_to_homeFragment)
+            CoroutineScope(Dispatchers.IO).launch {
+                val id = binding.editLoginId.text.toString()
+                val pw = binding.editLoginPw.text.toString()
+
+                logIn(User(null, id, pw))
+            }
+        }
+
+        logInViewModel.isLogin.observe(viewLifecycleOwner) { isLogIn ->
+            if (isLogIn) {
+                findNavController().navigate(R.id.action_fragment_login_to_homeFragment)
+            }
+        }
+
+        logInViewModel.errorMsg.observe(viewLifecycleOwner) { msg ->
+            val dialog = ErrorDialog(msg)
+            dialog.show(requireActivity().supportFragmentManager, "ErrorDialog")
+        }
+
+        logInViewModel.errorState.observe(viewLifecycleOwner) { error ->
+            if (error) {
+                val dialog = NoticeDialog(R.string.dialog_msg_error)
+                dialog.show(requireActivity().supportFragmentManager, "NoticeDialog")
+            }
+        }
+    }
+
+    private suspend fun logIn(user: User) {
+        try {
+            logInViewModel.login(user)
+        }catch (e: Exception) {
+            val dialog = NoticeDialog(R.string.dialog_permission)
+            dialog.show(requireActivity().supportFragmentManager, "NoticeDialog")
         }
     }
 
