77067b6dd7e7ddd3873e9d0ec5aa399edd520bdc
915dbfl <dbfl00915@naver.com>
2023-03-25 02:48:57 +0900

    [Feat] #29 네트워크 통신에 사용할 user model 수정

--- app/src/main/java/com/example/guessme/data/model/User.kt
+++ app/src/main/java/com/example/guessme/data/model/User.kt
@@ -1,7 +1,14 @@
 package com.example.guessme.data.model
 
+import android.os.Parcelable
+import com.squareup.moshi.Json
+import com.squareup.moshi.JsonClass
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+@JsonClass(generateAdapter = true)
 data class User(
     val uuid: Int?,
-    val id: Int,
-    val pw: Int
-)
+    @field:Json(name = "userId") val id: String,
+    @field:Json(name = "userPassword") val pw: String
+): Parcelable
