22b65d9cfec3c7ca3b2aa4743ebab708f47612b4
915dbfl <dbfl00915@naver.com>
2023-03-28 06:10:00 +0900

    [Feat] #30 인물 추가 화면 repository 구성 및 구현

---
+++ app/src/main/java/com/example/guessme/data/repository/AddPersonRepositoryImpl.kt
@@ -0,0 +1,55 @@
+package com.example.guessme.data.repository
+
+import com.example.guessme.data.api.RetrofitApi
+import com.example.guessme.data.model.Person
+import com.example.guessme.data.response.BaseResponseBody
+import com.example.guessme.domain.repository.AddPersonRepository
+import okhttp3.MediaType.Companion.toMediaTypeOrNull
+import okhttp3.MultipartBody
+import okhttp3.RequestBody
+import okhttp3.RequestBody.Companion.asRequestBody
+import okhttp3.RequestBody.Companion.toRequestBody
+import retrofit2.Response
+import java.io.File
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+class AddPersonRepositoryImpl @Inject constructor(
+    private val api: RetrofitApi
+): AddPersonRepository {
+
+    override suspend fun addPerson(token: String, person: Person, image: File?): Response<BaseResponseBody> {
+        var imageRequestBody: RequestBody?
+        var imageMultipartBody: MultipartBody.Part? = null
+        image?.let {
+            imageRequestBody = image.asRequestBody("image/jpeg".toMediaTypeOrNull())
+            imageMultipartBody = MultipartBody.Part.createFormData(name= "image", filename = image.name, body = imageRequestBody!!)
+        }
+
+        var voiceFile: File?
+        var voiceRequestBody: RequestBody?
+        var voiceMultipartBody: MultipartBody.Part? = null
+        person.voice?.let {
+            voiceFile = File(it.path!!)
+            voiceRequestBody = voiceFile!!.asRequestBody("audio/mp4".toMediaTypeOrNull())
+            voiceMultipartBody = MultipartBody.Part.createFormData(name= "image", filename = voiceFile!!.name, body = voiceRequestBody!!)
+        }
+
+        val nameRequestBody = person.name.toRequestBody("text/plain".toMediaTypeOrNull())
+        val relationRequestBody = person.relation.toRequestBody("text/plain".toMediaTypeOrNull())
+        val birthRequestBody = person.birth.toString().toRequestBody("text/plain".toMediaTypeOrNull())
+        val residenceRequestBody = person.residence.toRequestBody("text/plain".toMediaTypeOrNull())
+
+
+        return api.addPerson(
+            "Bearer $token",
+            imageMultipartBody,
+            voiceMultipartBody,
+            nameRequestBody,
+            relationRequestBody,
+            birthRequestBody,
+            residenceRequestBody
+        )
+    }
+}
