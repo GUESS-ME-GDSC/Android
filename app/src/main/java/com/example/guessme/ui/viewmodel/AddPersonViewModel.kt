d7809152be216915514ff05e3b44cc2943c29858
915dbfl <dbfl00915@naver.com>
2023-03-28 06:12:51 +0900

    [Feat] #30 addperson viewmodel을 통해 repository 속 인물 추가 함수 실행 코드 작성

--- app/src/main/java/com/example/guessme/ui/viewmodel/AddPersonViewModel.kt
+++ app/src/main/java/com/example/guessme/ui/viewmodel/AddPersonViewModel.kt
@@ -5,15 +5,33 @@ import android.media.MediaPlayer
 import android.media.MediaRecorder
 import android.net.Uri
 import android.os.Build
+import android.util.Log
 import androidx.annotation.RequiresApi
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.guessme.common.base.BasePlayer
 import com.example.guessme.common.base.BaseRecorder
+import com.example.guessme.data.model.Person
+import com.example.guessme.data.response.BaseResponseBody
+import com.example.guessme.domain.repository.AddPersonRepository
+import com.example.guessme.domain.repository.LocalRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.withContext
+import retrofit2.Response
+import java.io.File
+import javax.inject.Inject
+import java.lang.Exception
 
-class AddPersonViewModel: ViewModel() {
+@HiltViewModel
+class AddPersonViewModel @Inject constructor(
+    private val addPersonRepository: AddPersonRepository,
+    private val localRepository: LocalRepository
+): ViewModel() {
     private var _imageUri: Uri? = null
+    val imageUri: Uri? get() = _imageUri
     private var _basePlayer: BasePlayer? = null
     private val basePlayer get() = _basePlayer!!
     private var _baseRecorder: BaseRecorder? = null
@@ -22,6 +40,10 @@ class AddPersonViewModel: ViewModel() {
     val recordStatus: LiveData<Boolean> get() = _recordStatus
     private var _fileName: String? = null
     val fileName get() = _fileName
+    private val _addSuccess = MutableLiveData(false)
+    val addSuccess: LiveData<Boolean> = _addSuccess
+    private val _errorState = MutableLiveData<Int>()
+    val errorState: LiveData<Int> = _errorState
 
     fun setImage(image: Uri) {
         _imageUri = image
@@ -61,5 +83,28 @@ class AddPersonViewModel: ViewModel() {
 
     fun setFileName(name: String) {
         _fileName = name
+    }
+
+    suspend fun addPerson(person: Person, image: File?) {
+        try {
+            val token = withContext(Dispatchers.IO){
+                localRepository.getToken().first()
+            }
+            val response: Response<BaseResponseBody> = addPersonRepository.addPerson(token, person, image)
+            val status = response.body()?.status
+            Log.d("status", status.toString())
+            Log.d("status", response.body()!!.data)
+
+            if((status == 200) and response.isSuccessful) {
+                _addSuccess.postValue(true)
+            }else if (status == 401) {
+                _errorState.postValue(401)
+            } else {
+                _errorState.postValue(status!!)
+            }
+        }catch (e: Exception) {
+            Log.e("e", e.toString())
+            _errorState.postValue(1)
+        }
     }
 }
