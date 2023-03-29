5ebbbba1a8ea9aba82afc2998d681a606071c1ad
915dbfl <dbfl00915@naver.com>
2023-03-29 10:36:36 +0900

    [Feat] #31 InfoList data class가 api 요청 응답에 사용될 수 있도록 수정

--- app/src/main/java/com/example/guessme/data/model/InfoList.kt
+++ app/src/main/java/com/example/guessme/data/model/InfoList.kt
@@ -1,9 +1,12 @@
 package com.example.guessme.data.model
 
 import android.os.Parcelable
+import com.squareup.moshi.Json
+import com.squareup.moshi.JsonClass
 import kotlinx.parcelize.Parcelize
 
 @Parcelize
+@JsonClass(generateAdapter = true)
 data class InfoList(
-    val data: List<Info>
+    @field:Json(name = "info") val data: List<Info>
 ): Parcelable
