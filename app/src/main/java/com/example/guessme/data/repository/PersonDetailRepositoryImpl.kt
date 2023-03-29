e470916fceadd92e82aa922957c6f6aa753b6de7
915dbfl <dbfl00915@naver.com>
2023-03-29 10:40:13 +0900

    [Feat] #31 repository 속 인물 정보 추가 응답에 basenullresponsebody 적용

--- app/src/main/java/com/example/guessme/data/repository/PersonDetailRepositoryImpl.kt
+++ app/src/main/java/com/example/guessme/data/repository/PersonDetailRepositoryImpl.kt
@@ -2,7 +2,7 @@ package com.example.guessme.data.repository
 
 import com.example.guessme.data.api.RetrofitApi
 import com.example.guessme.data.model.InfoList
-import com.example.guessme.data.response.BaseResponseBody
+import com.example.guessme.data.response.BaseNullResponseBody
 import com.example.guessme.data.response.PersonResponse
 import com.example.guessme.domain.repository.PersonDetailRepository
 import retrofit2.Response
@@ -14,7 +14,7 @@ class PersonDetailRepositoryImpl @Inject constructor(
     private val api: RetrofitApi
 ): PersonDetailRepository {
 
-    override suspend fun addInfo(token: String, id: Int, info: InfoList): Response<BaseResponseBody> {
+    override suspend fun addInfo(token: String, id: Int, info: InfoList): Response<BaseNullResponseBody> {
         return api.addInfo(token, id, info)
     }
 
--- app/src/main/java/com/example/guessme/domain/repository/PersonDetailRepository.kt
+++ app/src/main/java/com/example/guessme/domain/repository/PersonDetailRepository.kt
@@ -1,12 +1,13 @@
 package com.example.guessme.domain.repository
 
 import com.example.guessme.data.model.InfoList
+import com.example.guessme.data.response.BaseNullResponseBody
 import com.example.guessme.data.response.BaseResponseBody
 import com.example.guessme.data.response.PersonResponse
 import retrofit2.Response
 
 interface PersonDetailRepository {
-    suspend fun addInfo(token: String, id: Int, info: InfoList): Response<BaseResponseBody>
+    suspend fun addInfo(token: String, id: Int, info: InfoList): Response<BaseNullResponseBody>
 
     suspend fun getPerson(token: String, id: Int): Response<PersonResponse>
 }
