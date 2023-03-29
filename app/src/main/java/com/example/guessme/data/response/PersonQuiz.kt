5f30f0d70573d568be86283690e8d8fe6008a760
915dbfl <dbfl00915@naver.com>
2023-03-29 18:56:13 +0900

    [Fix] #33 퀴즈 요청 responsebody 수정

---
+++ app/src/main/java/com/example/guessme/data/response/PersonQuiz.kt
@@ -0,0 +1,14 @@
+package com.example.guessme.data.response
+
+import com.squareup.moshi.Json
+import com.squareup.moshi.JsonClass
+
+@JsonClass(generateAdapter = true)
+data class PersonQuiz(
+    @field:Json(name = "image")
+    val image: String?,
+    @field:Json(name = "quizList")
+    val quizList: List<Quiz>,
+    @field:Json(name = "voice")
+    val voice: String?
+)
