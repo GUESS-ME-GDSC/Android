38c788dae1e3c9ca37df7a44367a670351014569
915dbfl <dbfl00915@naver.com>
2023-03-29 20:44:40 +0900

    [Feat] #34 카메라 퍼미션 적용

--- app/src/main/java/com/example/guessme/common/util/Constants.kt
+++ app/src/main/java/com/example/guessme/common/util/Constants.kt
@@ -2,6 +2,7 @@ package com.example.guessme.common.util
 
 object Constants {
     const val REQUIRED_GALLERY_PERMISSION = android.Manifest.permission.READ_MEDIA_IMAGES
+    const val REQUIRED_CAMERA_PERMISSION = android.Manifest.permission.CAMERA
     const val REQUIRED_RECORD_AUDIO = android.Manifest.permission.RECORD_AUDIO
     const val DATASTORE_NAME = "preferences_datastore"
 }
