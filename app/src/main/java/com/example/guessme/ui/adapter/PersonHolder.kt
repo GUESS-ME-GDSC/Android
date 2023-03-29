1ce7232354d182720b6ced084bbd2c992d113b5e
915dbfl <dbfl00915@naver.com>
2023-03-29 11:45:49 +0900

    [Feat] #30 인물 리스트 recyclerview의 holder와 adapter 대상 personpreview 객체로 수정

--- app/src/main/java/com/example/guessme/ui/adapter/PersonHolder.kt
+++ app/src/main/java/com/example/guessme/ui/adapter/PersonHolder.kt
@@ -1,18 +1,21 @@
 package com.example.guessme.ui.adapter
 
+import android.net.Uri
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
 import com.example.guessme.data.model.Person
+import com.example.guessme.data.response.PersonPreview
 import com.example.guessme.databinding.ItemPersonPreviewBinding
 
 class PersonHolder(private val binding: ItemPersonPreviewBinding):
     RecyclerView.ViewHolder(binding.root) {
 
-        fun bind(person: Person) {
+        fun bind(person: PersonPreview) {
             val name = person.name
             val relation = person.relation
             val favorite = person.favorite
             val score = person.score
+            val imageUri = Uri.parse(person.image)
 
             if (favorite) {
                 binding.imagePreviewFavoriteTrue.visibility = View.VISIBLE
@@ -24,6 +27,6 @@ class PersonHolder(private val binding: ItemPersonPreviewBinding):
             binding.txtPreviewRelation.text = relation
             binding.txtPreviewScore.text = "$score%"
             binding.progressPreviewScore.progress = score!!
-            //이미지 관련 처리 필요
+            binding.imagePreviewPerson.setImageURI(imageUri)
         }
 }
