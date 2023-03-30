3c32ff851a39d0cab633f01ad0dadcd51494d737
915dbfl <dbfl00915@naver.com>
2023-03-30 19:48:12 +0900

    [Feat] #34 채점 요청 responsebody 정의

--- app/src/main/java/com/example/guessme/data/response/Quiz.kt
+++ app/src/main/java/com/example/guessme/data/response/Quiz.kt
@@ -1,12 +1,15 @@
 package com.example.guessme.data.response
 
+import android.os.Parcelable
 import com.squareup.moshi.Json
 import com.squareup.moshi.JsonClass
+import kotlinx.parcelize.Parcelize
 
 @JsonClass(generateAdapter = true)
+@Parcelize
 data class Quiz(
     @field:Json(name = "answer")
     val answer: String,
     @field:Json(name = "question")
     val question: String
-)
+): Parcelable
