902c6313cb772d1a8681d5edf741acb9db470426
915dbfl <dbfl00915@naver.com>
2023-03-31 02:14:48 +0900

    [Feat] #34 퀴즈 결과 등록 관련 repository 정의

--- app/src/main/java/com/example/guessme/data/repository/ScoreRepositoryImpl.kt
+++ app/src/main/java/com/example/guessme/data/repository/ScoreRepositoryImpl.kt
@@ -1,6 +1,8 @@
 package com.example.guessme.data.repository
 
 import com.example.guessme.data.api.RetrofitApi
+import com.example.guessme.data.model.NewScore
+import com.example.guessme.data.response.BaseResponseBody
 import com.example.guessme.data.response.PersonResponse
 import com.example.guessme.domain.repository.ScoreRepository
 import retrofit2.Response
@@ -13,5 +15,9 @@ class ScoreRepositoryImpl @Inject constructor(
 ): ScoreRepository {
     override suspend fun getPerson(token: String, id: Int): Response<PersonResponse> {
         return api.getPerson(token, id)
+    }
+
+    override suspend fun patchNewScore(token: String, newScore: NewScore): Response<BaseResponseBody> {
+        return api.patchNewScore(token, newScore)
     }
 }
