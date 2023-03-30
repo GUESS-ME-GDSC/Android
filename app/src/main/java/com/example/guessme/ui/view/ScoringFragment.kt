2c9cb52f59563113e412dd2b56d21cdc6f5a309a
915dbfl <dbfl00915@naver.com>
2023-03-30 21:00:59 +0900

    [Feat] #34 퀴즈 채점 로직 구현

--- app/src/main/java/com/example/guessme/ui/view/ScoringFragment.kt
+++ app/src/main/java/com/example/guessme/ui/view/ScoringFragment.kt
@@ -1,16 +1,87 @@
 package com.example.guessme.ui.view
 
+import android.net.Uri
+import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
 import com.example.guessme.R
 import com.example.guessme.common.base.BaseFragment
 import com.example.guessme.databinding.FragmentScoringBinding
+import com.example.guessme.ui.dialog.NoticeDialog
+import com.example.guessme.ui.viewmodel.ScoringViewModel
+import com.example.guessme.ui.viewmodel.StartQuizViewModel
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
+@AndroidEntryPoint
 class ScoringFragment : BaseFragment<FragmentScoringBinding>(R.layout.fragment_scoring) {
+    private val startQuizViewModel: StartQuizViewModel by activityViewModels()
+    private val scoringFragmentArgs: ScoringFragmentArgs by navArgs()
+    private val scoringViewModel: ScoringViewModel by viewModels()
+
     override fun getFragmentBinding(
         inflater: LayoutInflater,
         container: ViewGroup?
     ): FragmentScoringBinding {
         return FragmentScoringBinding.inflate(inflater, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setObserver()
+        init()
+
+        CoroutineScope(Dispatchers.IO).launch {
+            scoringViewModel.quizScoring(scoringFragmentArgs.imageUri, scoringFragmentArgs.quiz.answer)
+        }
+    }
+
+    private fun setObserver() {
+        scoringViewModel.result.observe(viewLifecycleOwner) { result ->
+            if (result) {
+                startQuizViewModel.setCorrect()
+                binding.txtQuizResult.setText(R.string.quiz_result_correct)
+            } else {
+                binding.txtQuizResult.setText(R.string.quiz_result_wrong)
+            }
+        }
+
+        scoringViewModel.quizScoring.observe(viewLifecycleOwner) { quizScoring ->
+            if (! quizScoring) {
+                val dialog = NoticeDialog(R.string.dialog_msg_error)
+                dialog.show(requireActivity().supportFragmentManager, "NoticeDialog")
+            }
+        }
+    }
+
+    private fun init() {
+        val uri = Uri.parse(scoringFragmentArgs.imageUri)
+        binding.imageQuizUserAnswer.setImageURI(uri)
+
+        val quiz = startQuizViewModel.getCurQuiz()
+        Log.d("quiz", quiz.toString())
+        binding.txtQuizAnswerKey.text = quiz.question
+        binding.txtQuizAnswerValue.text = quiz.answer
+
+        if (startQuizViewModel.isQuizLast()) {
+            binding.btnQuizAnswerNext.visibility = View.GONE
+        }
+
+        binding.btnQuizAnswerNext.setOnClickListener {
+            startQuizViewModel.increaseCur()
+            findNavController().navigate(R.id.action_fragment_scoring_to_fragment_quiz)
+        }
+
+        binding.btnQuizStop.setOnClickListener {
+            findNavController().navigate(R.id.action_fragment_scoring_to_scoreFragment)
+        }
     }
 }
---
+++ app/src/main/java/com/example/guessme/ui/viewmodel/ScoringViewModel.kt
@@ -0,0 +1,49 @@
+package com.example.guessme.ui.viewmodel
+
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.example.guessme.data.response.ScoringResponseBody
+import com.example.guessme.domain.repository.LocalRepository
+import com.example.guessme.domain.repository.ScoringRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import retrofit2.Response
+import javax.inject.Inject
+
+@HiltViewModel
+class ScoringViewModel @Inject constructor(
+    private val localRepository: LocalRepository,
+    private val scoringRepository: ScoringRepository
+): ViewModel() {
+    private val _result = MutableLiveData<Boolean>()
+    val result: LiveData<Boolean> = _result
+    private val _quizScoring = MutableLiveData<Boolean>()
+    val quizScoring: LiveData<Boolean> = _quizScoring
+
+    suspend fun quizScoring(image: String, infoValue: String) {
+        try {
+            val token = withContext(Dispatchers.IO) {
+                localRepository.getToken().first()
+            }
+
+            val response: Response<ScoringResponseBody> = scoringRepository.quizScoring("Bearer $token", image, infoValue)
+            val status = response.body()?.status
+
+            if((status == 200) and response.isSuccessful) {
+                _result.postValue( response.body()!!.data!!)
+            } else {
+                _quizScoring.postValue(false)
+            }
+        } catch (e: java.lang.Exception) {
+            Log.d("e", e.toString())
+            _quizScoring.postValue(false)
+        }
+    }
+
+}
