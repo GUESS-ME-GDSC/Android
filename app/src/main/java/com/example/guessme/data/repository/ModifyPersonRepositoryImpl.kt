8692afe5948b5e6761f14b6d97cfaf16b243804b
915dbfl <dbfl00915@naver.com>
2023-03-31 09:19:38 +0900

    [Feat] #32 인물 삭제 및 수정 관련 repository 생성 및 삭제 함수 정의

--- app/src/main/java/com/example/guessme/data/repository/ModifyPersonRepositoryImpl.kt
+++ app/src/main/java/com/example/guessme/data/repository/ModifyPersonRepositoryImpl.kt
@@ -1,4 +1,17 @@
 package com.example.guessme.data.repository
 
-class ModifyPersonRepositoryImpl {
+import com.example.guessme.data.api.RetrofitApi
+import com.example.guessme.data.response.BaseNullResponseBody
+import com.example.guessme.domain.repository.ModifyPersonRepository
+import retrofit2.Response
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+class ModifyPersonRepositoryImpl @Inject constructor(
+    private val api: RetrofitApi
+): ModifyPersonRepository {
+    override suspend fun deletePerson(token: String, id: Int): Response<BaseNullResponseBody> {
+        return api.deletePerson(token, id)
+    }
 }
--- app/src/main/java/com/example/guessme/domain/repository/ModifyPersonRepository.kt
+++ app/src/main/java/com/example/guessme/domain/repository/ModifyPersonRepository.kt
@@ -1,4 +1,8 @@
 package com.example.guessme.domain.repository
 
+import com.example.guessme.data.response.BaseNullResponseBody
+import retrofit2.Response
+
 interface ModifyPersonRepository {
+    suspend fun deletePerson(token: String, id: Int): Response<BaseNullResponseBody>
 }
